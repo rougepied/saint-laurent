f4cc6532c7796b24db6680630b2ffe553fc3ab71
unknown <AMA05294@REN1DSK0046.ouest.teamlog.intra>
2015-10-28 14:49:22 +0100

    add jshint to brunch-config

--- brunch-config.js
+++ brunch-config.js
@@ -5,14 +5,29 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: {
-        'vendor.js': /^bower_components/,
-        'app.js': 'app/**/*.js'
+        "vendor.js": /^bower_components/,
+        "app.js": "app/**/*.js"
       }
     }
   },
   plugins: {
     uglify: {
       mangle: false
+    },
+    jshint: {
+      pattern: /^app[\\\/].*\.js$/,
+      options: {
+        esnext: true,
+        strict: false,
+        globalstrict: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        predef: ["document", "window", "moment", "HTMLElement", "fetch"],
+        undef: true,
+        unused: true
+      },
+      warnOnly: true
     }
   }
 }
